a5a8fdcce465b265426dcc89921083b41bf0bbd4
Simonas Kareiva <simonas@5grupe.lt>
2017-05-07 19:27:49 +0300

    Rename app LT->EE

---
+++ app/src/main/java/com/ee/erau/hamradio/quiz/activity/Settings.java
@@ -0,0 +1,18 @@
+package com.ee.erau.hamradio.quiz.activity;
+import roboguice.activity.RoboPreferenceActivity;
+import android.os.Bundle;
+
+import com.ee.erau.hamradio.quiz.R;
+
+public class Settings extends RoboPreferenceActivity {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings); 
+	}
+ 
+
+
+}
